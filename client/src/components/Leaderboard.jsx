0349a3c877c87f0d150c7b387aa2ecc217f36914
LordByron47 <55666548+LordByron47@users.noreply.github.com>
2020-09-11 13:42:23 -0700

    server is working?

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
 export class Leaderboard extends Component {
+
     render() {
+        console.log(this.props.allScores);
         return (
             <div>
-                
+                a
             </div>
         )
     }
--- server/src/resolver.js
+++ server/src/resolver.js
@@ -5,16 +5,26 @@ const LEADERBOARD_CHANNEL = 'LEADERBOARD_CHANNEL'
 
 const resolvers = {
   Query: {
-    scores (root, args, context) {
+    scores(root, args, context) {
       return scores
     }
   },
 
   Mutation: {
-    sendScore (root, { name, points, operation }, { pubsub }) {
+    sendScore(root, { name, points, operation }, { pubsub }) {
       const score = { id: scores.length + 1, name, points, operation }
+      if (scores.length > 10) {
+        if (score.points > scores[scores.length - 1]) {
+          scores.push(score);
+          scores.sort(sortScores);
+          scores.pop();
+        }
+      }
+      else{
+        scores.push(score);
+        scores.sort(sortScores);
+      }
 
-      scores.push(score)
       pubsub.publish('LEADERBOARD_CHANNEL', { scoreAdded: score })
 
       return score
@@ -28,6 +38,16 @@ const resolvers = {
       }
     }
   }
+}
+
+function sortScores(firstEl, secondEl) {
+  if (firstEl.points > secondEl.points) {
+    return -1;
+  }
+  else if (firstEl.points < secondEl.points) {
+    return 1;
+  }
+  else return 0;
 }
 
 module.exports = resolvers
